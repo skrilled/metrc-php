bc66641c69ce28047564bfa79fca4c9f21517625
Brandon DuBois <brandon@canapass.com>
2018-12-18 12:16:14 -0800

    testing

--- test.php
+++ test.php
@@ -2,4 +2,10 @@
 
 require_once __DIR__ .'/vendor/autoload.php';
 
-$api = new \MetrcApi\MetrcApi('', '', '');
+$api = new \MetrcApi\MetrcApi(
+    'FusVbe4Yv6W1DGNuxKNhByXU6RO6jSUPcbRCoRDD98VNXc4D',
+    'cbzfcdmDlhtecC3vaRYTWAr8-naRsPQMu09dHm6-IscGVDHY',
+    'CAL17-0000005'
+);
+
+var_dump($api->getRooms());
